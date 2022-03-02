574cbcb795f34a57446d0de5a24ddee1e0adba3e
Simon Andersen <sandersen98@live.dk>
2022-03-02 09:39:52 +0100

    project commit

---
+++ Registr/RegistrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrApp.swift
+//  Registr
+//
+//  Created by Simon Andersen on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RegistrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
