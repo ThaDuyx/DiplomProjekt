8474ba5869ec8d72f8ae237ac44228f4018a2ba5
ChrisMizz <Christofferdetlef@gmail.com>
2022-03-10 12:53:16 +0100

    Updated the localizable Fixed naming of the alert struct

--- Registr/Login/Onboarding/OnboardingFlowControllerView.swift
+++ Registr/Login/Onboarding/OnboardingFlowControllerView.swift
@@ -7,17 +7,18 @@
 
 import SwiftUI
 
-struct PurchaseDetail: Identifiable {
+struct AlertDetail: Identifiable {
     var id: Int
-    let trackingId: String
-    let numberOfItems: String
+    let alertTitle: String
+    let alertMessage: String
 }
 
 struct OnboardingFlowControllerView: View {
     @AppStorage("currentPage") var currentPage = 1
+    @AppStorage("dataAccess") var dataAccess = false
     @State private var isShowingDetailView = false
     @State private var buttonText: String = ""
-    @State private var purchaseDetail: PurchaseDetail?
+    @State private var alertDetail: AlertDetail?
 
     private var alertTitle: String = ""
     private var alertMessage: String = ""
@@ -25,7 +26,7 @@ struct OnboardingFlowControllerView: View {
     
     let pages = [
         OnboardFlowView(id: 1, title: "onboard_flow_page_one_title", details: "onboard_flow_page_one_description", image: "checkmark.shield.fill", buttonText: "onboard_flow_page_one_button_text"),
-        OnboardFlowView(id: 2, title: "onboard_flow_page_two_title", details: "onboard_flow_page_one_description", image: "bell.fill", buttonText: "onboard_flow_page_two_button_text"),
+        OnboardFlowView(id: 2, title: "onboard_flow_page_two_title", details: "onboard_flow_page_two_description", image: "bell.fill", buttonText: "onboard_flow_page_two_button_text"),
         OnboardFlowView(id: 3, title: "onboard_flow_page_three_title", details: "onboard_flow_page_three_description", image: "checklist", buttonText: "onboard_flow_page_three_button_text")
     ]
     
@@ -44,16 +45,16 @@ struct OnboardingFlowControllerView: View {
                             withAnimation(.easeInOut) {
                                 if currentPage <= totalPages {
                                     if currentPage == 1 {
-                                        purchaseDetail = PurchaseDetail(
+                                        alertDetail = AlertDetail(
                                             id: 1,
-                                            trackingId: "alert 1",
-                                            numberOfItems: "alert 1"
+                                            alertTitle: "Adgang til din data",
+                                            alertMessage: "Giv appen lov til at tilgå data om dig."
                                         )
                                     } else if currentPage == 2 {
-                                        purchaseDetail = PurchaseDetail(
+                                        alertDetail = AlertDetail(
                                             id: 2,
-                                            trackingId: "alert 2",
-                                            numberOfItems: "alert 2"
+                                            alertTitle: "Placeholder title",
+                                            alertMessage: "Placeholder message"
                                         )
                                     } else {
                                         currentPage += 1
@@ -68,13 +69,19 @@ struct OnboardingFlowControllerView: View {
                 ,alignment: .bottom
             )
         }
-        .alert(item: $purchaseDetail, content: { content in
+        .alert(item: $alertDetail, content: { content in
             Alert(
-                title: Text(content.trackingId),
-                message: Text(content.numberOfItems),
-                primaryButton: .default(Text("Okay"), action: {
+                title: Text((content.alertTitle).localize),
+                message: Text((content.alertMessage.localize).localize),
+                primaryButton: .default(Text("Godkend"), action: {
+                    if currentPage == 1 {
+                        dataAccess = true
+                    } else if currentPage == 2 {
+                        // TODO: - This is placeholder for push notification - CAD
+                        let _ = print("You have given the app access to use notifications.")
+                    }
                     currentPage += 1
-                }), secondaryButton: .cancel(Text("Cancel"), action: {
+                }), secondaryButton: .cancel(Text("Ikke godkendt"), action: {
                     currentPage += 1
                 })
             )
