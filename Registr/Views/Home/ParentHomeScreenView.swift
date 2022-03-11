65eb1dafbc8473ea2d3cb8cd499113fd03834d36
ChrisMizz <Christofferdetlef@gmail.com>
2022-03-11 12:00:49 +0100

    Again...

--- Registr/Views/Home/ParentHomeScreenView.swift
+++ Registr/Views/Home/ParentHomeScreenView.swift
@@ -9,7 +9,58 @@ import SwiftUI
 
 struct ParentHomeScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Resources.BackgroundGradient.backgroundGradient
+                    .ignoresSafeArea()
+                Form {
+                    Section {
+                        CardStack()
+                    }
+                }
+            }
+            .navigationTitle("Dine børn")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct CardStack: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    VStack {
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                    }
+                    .frame(width: 90, height: 100)
+                    .background(Color.red)
+                    Spacer()
+                }
+                .foregroundColor(Color.blue)
+                .frame(alignment: .leading)
+                Spacer()
+                HStack {
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            Text("Navn:")
+                            Text("Simon Andersen")
+                        }
+                        VStack(alignment: .leading) {
+                            Text("Klasse:")
+                            Text("8.Y")
+                        }
+                        VStack(alignment: .leading) {
+                            Text("Email:")
+                            Text("Email@email.dk")
+                        }
+                    }
+                }
+                .frame(alignment: .center)
+            }
+        }
     }
 }
 
