5ac5c029c89ca12d0eefdb6a4c95c62d67f231a8
Simon Andersen <sandersen98@live.dk>
2022-03-23 11:35:16 +0100

    Login route implemented and small fixed

--- Registr/Views/TabView/TabViewView.swift
+++ Registr/Views/TabView/TabViewView.swift
@@ -9,32 +9,50 @@ import SwiftUI
 
 struct TabViewView: View {
     @State private var role: String = ""
+    @State private var userRole = UserManager.shared.user?.role
     
     var body: some View {
         TabView {
-            if role == "parent" {
+            
+            switch userRole {
+            case .teacher:
+                TeacherHomeScreenView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Start")
+                    }
+                
+                RegisterView()
+                    .tabItem {
+                        Image(systemName: "square.and.pencil")
+                        Text("Registrer")
+                    }
+                ProfileView()
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("Profil")
+                    }
+            case .parent:
                 ParentHomeScreenView()
                     .tabItem {
                         Image(systemName: "house")
                         Text("Start")
                     }
-            } else {
-            TeacherHomeScreenView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Start")
-                }
+            case .headmaster:
+                // TODO: Make headmaster view
+                ParentHomeScreenView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Start")
+                    }
+            case .none:
+                ParentHomeScreenView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Start")
+                    }
+
             }
-            RegisterView()
-                .tabItem {
-                    Image(systemName: "square.and.pencil")
-                    Text("Registrer")
-                }
-            ProfileView()
-                .tabItem {
-                    Image(systemName: "person")
-                    Text("Profil")
-                }
         }
         .accentColor(Resources.Color.Colors.darkBlue)
     }
